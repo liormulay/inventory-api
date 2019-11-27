ff75430d3222df59242315dc2483f85f52a4748e
Lior Mulay <lior@superup.me>
2019-11-27 12:05:00 +0200

    Revert "remove quantity from item"
    This reverts commit cee4dc1c

--- src/main/java/com/inventory/inventory/api/item/Item.java
+++ src/main/java/com/inventory/inventory/api/item/Item.java
@@ -8,13 +8,15 @@ public class Item {
     @Id
     private String itemNo;
     private String itemDetails;
+    private int quantity;
 
     public Item() {
     }
 
-    public Item(String itemNo, String itemDetails) {
+    public Item(String itemNo, String itemDetails, int quantity) {
         this.itemNo = itemNo;
         this.itemDetails = itemDetails;
+        this.quantity = quantity;
     }
 
     public String getItemNo() {
@@ -33,4 +35,11 @@ public class Item {
         this.itemDetails = itemDetails;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
